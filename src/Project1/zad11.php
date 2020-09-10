4d7a2f1ca90368ad17d49cb9d3e194caa4c0e3cb
lukasz <zidane574@wp.pl>
2020-09-10 23:18:45 +0200

    zadanie 11

--- src/Project1/zad11.php
+++ src/Project1/zad11.php
@@ -6,11 +6,36 @@ Na przykład [2, 4, 6, 2, 5] powinno zwrócić 13, ponieważ wybieramy 2, 6 i 5.
 powinno zwrócić 10, ponieważ wybieramy 5 i 5.
 */
 
-$array = [5, 1, 1, 5];
-($array);
-foreach ($array as $linia =>$item){
+$array = [2, 4, 6, 2, 5];
+
+echo maxSum($array);
+
+function maxSum($array)
+{
+    $excl = 0;
+    $incl = $array[0];
+    for ($i = 1; $i < sizeof($array) ;$i++)
+    {
+        $temp = $incl;
+        $incl = max(max($excl + $array[$i], $array[$i]), $incl);
+        $excl = $temp;
+    }
+    return $incl;
+}
+
+
+
+/*foreach ($array as $linia =>$item){
 
     echo $linia." ".$item.PHP_EOL;
+
+    if(sizeof($array)== 3){
+
+        echo $array[0]+$array[2];
+    } else if(sizeof($array)== 4){
+
+
+    }
 
     foreach ($array as $linia1 =>$item1){
         if($linia != $linia1){
@@ -19,4 +44,4 @@ foreach ($array as $linia =>$item){
             }
         }
     }
-}
+}*/
